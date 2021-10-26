dd38953a658a3d987471224d63e85752c6a130fe
vrvrv <pauljin95@gmail.com>
2021-10-26 13:36:21 +0900

    Initial commit

---
+++ train.py
@@ -0,0 +1,22 @@
+#
+# https://github.com/ashleve/lightning-hydra-template
+#
+
+import hydra
+from omegaconf import DictConfig
+
+
+@hydra.main(config_path="configs/", config_name="config.yaml")
+def main(config: DictConfig):
+    from src import utils
+    from src.train import train
+
+    if config.get("print_config"):
+        utils.print_config(config, resolve=True)
+
+    # job type is train or test
+    train(config)
+
+
+if __name__ == "__main__":
+    main()
